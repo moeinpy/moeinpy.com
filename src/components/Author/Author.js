2a41d9c471f37c62568f12a4ecc80b274ba3bd8c
Moein Pahlavan Yali <moein.pahlavan@gmail.com>
2020-01-08 13:19:26 -0800

    improve post author

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -23,8 +23,8 @@ export const PureAuthor = ({ author, showBio, showTwitter }) => {
             </Link>
             {showTwitter && ' '}
             {showTwitter && (
-              <a style={{ fontWeight: 500 }} href="https://twitter.com/victorczhou" target="_blank" rel="noopener noreferrer">
-                @victorczhou
+              <a style={{ fontWeight: 500 }} href="https://twitter.com/moeinpy" target="_blank" rel="noopener noreferrer">
+                @moeinpy
               </a>
             )}
           </h4>
